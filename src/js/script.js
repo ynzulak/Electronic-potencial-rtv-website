12af6aefe1e233dbd8b501b776286278d52bbec2
ynzulak <maksymilian.a.kaluzny@gmail.com>
2023-04-01 20:45:58 +0200

    Fixed HTML

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+{/* <div class="product">
+                                <div class="product-header">
+                                    <h2>Smartfon Apple iPhone 13 128GB - 6,1" - 12 Mpix - niebieski</h2>
+                                </div>
+                                <div class="product-img"><img src="/src/img/iphone13.jpg" alt=""></div>
+                                <div class="price-box">
+                                    <div class="full-price">
+                                        <div class="prices">
+                                            <div class="price-with-cents-and-currency"><span class="price">3999</span>
+                                                <span class="cents-with-currency">
+                                                    <span class="cents">00</span>
+                                                    <span class="'currency">zł</span></span>
+                                            </div>
+                                            <div class="price-discount">
+                                                <div class="old-price">
+                                                    <span class="whole">4499</span>
+                                                    <span class="spearator">,</span>
+                                                    <span class="cents">00</span>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="installment"><span>224,95 zł x 20 rat 0%</span></div>
+                                    <div class="add-to-cart"><button class="add-to-cart-button"><span>DO
+                                                KOSZYKA</span></button></div>
+                                </div>
+                            </div>
+const currentProduct = products */}
