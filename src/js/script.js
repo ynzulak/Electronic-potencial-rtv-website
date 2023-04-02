3d47cda7a65e7b085fb553c89d0c2ada06790e78
ynzulak <maksymilian.a.kaluzny@gmail.com>
2023-04-02 23:42:09 +0200

    Rendering images not working

--- src/js/script.js
+++ src/js/script.js
@@ -2,49 +2,43 @@ import { products } from './products.js'
 
 const productsSection = document.querySelector('.products-list')
 
-const priceCentsCut = function() {
-    const numToString = products.price.toString();
-    products.price = products.price.slice(0, -3)
-}
-
-const renderProducts = function () {
+const renderProducts = function (products) {
 	products.forEach(products => {
 		const newProduct = document.createElement('div')
-		newProduct.className = '.product'
-		newProduct.innerHTML = `
-        <div class="product">
+		newProduct.className = `product ${products.sale ? '' : 'not-on-sale'}  ${
+			products.installment ? '' : 'no-installment'
+		}`
+		newProduct.innerHTML += `
         <div class="product-header">
             <h2>${products.name}</h2>
         </div>
-        <div class="product-img"><img src="/src/img/iphone13.jpg" alt=""></div>
+        <div class="product-img"><img src="${products.image}" alt=""></div>
         <div class="price-box">
             <div class="full-price">
                 <div class="prices">
                     <div class="price-with-cents-and-currency"><span class="price">${products.price}</span>
                         <span class="cents-with-currency">
-                            <span class="cents">00</span>
+                            <span class="cents">${products.cents}</span>
                             <span class="'currency">zł</span></span>
                     </div>
                     <div class="price-discount">
                         <div class="old-price">
-                            <span class="whole">4499</span>
+                            <span class="whole">${products.price + products.saleAmount}</span>
                             <span class="spearator">,</span>
-                            <span class="cents">00</span>
+                            <span class="cents">${products.cents}</span>
                         </div>
 
                     </div>
                 </div>
             </div>
-            <div class="installment"><span>224,95 zł x 20 rat 0%</span></div>
+            <div class="installment"><span>${products.installment} zł x 20 rat 0%</span></div>
             <div class="add-to-cart"><button class="add-to-cart-button"><span>DO
                         KOSZYKA</span></button></div>
         </div>
-    </div>
         `
 
 		productsSection.appendChild(newProduct)
 	})
 }
 
-renderProducts()
-console.log(renderProducts())
+document.onload = renderProducts(products)
