acaaf5f8bdd3c9d9ff6ee7b28ed61dd87d2fca70
ynzulak <maksymilian.a.kaluzny@gmail.com>
2023-04-19 15:10:43 +0200

    Added rendering products when clicking on the category buttons

--- src/js/script.js
+++ src/js/script.js
@@ -7,28 +7,10 @@ const categoryBtn = document.querySelectorAll('.btn')
 const categoryItem = document.querySelectorAll('.cat-item')
 const modelItem = document.querySelectorAll('.model-item')
 const categoriesItems = document.querySelector('.categories-container')
-
-categoryItem.forEach(btn =>
-	btn.addEventListener('click', function (e) {
-		const category = e.target.dataset.category
-		const recomended = e.target.dataset.recomended
-		const selectedCategoryProducts = products.filter(item => {
-			if (item.category === category) return item
-		})
-		console.log(selectedCategoryProducts)
-
-		categoryItem.forEach(item => item.classList.remove('active'))
-		this.classList.add('active')
-	})
-)
-modelItem.forEach(btn =>
-	btn.addEventListener('click', function () {
-		modelItem.forEach(item => item.classList.remove('active'))
-		this.classList.add('active')
-	})
-)
+const recomendedBtn = document.querySelector('.recomended-btn')
 
 const renderProducts = function (products) {
+	productsSection.innerHTML = ''
 	products.forEach(products => {
 		const newProduct = document.createElement('div')
 		newProduct.className = `product ${products.sale ? '' : 'not-on-sale'}  ${
@@ -37,43 +19,70 @@ const renderProducts = function (products) {
 		newProduct.innerHTML = `
         <div class="product-header">
             <h2>${products.name}</h2>
-        </div>
-        <div class="product-img"><img src="${products.image}"></div>
-        <div class="price-box">
+            </div>
+            <div class="product-img"><img src="${products.image}"></div>
+            <div class="price-box">
             <div class="full-price">
-                <div class="prices">
-                    <div class="price-with-cents-and-currency"><span class="price">${products.price}</span>
-                        <span class="cents-with-currency">
-                            <span class="cents">${products.cents}</span>
-                            <span class="'currency">zł</span></span>
-                    </div>
+            <div class="prices">
+            <div class="price-with-cents-and-currency"><span class="price">${products.price}</span>
+            <span class="cents-with-currency">
+            <span class="cents">${products.cents}</span>
+            <span class="'currency">zł</span></span>
+            </div>
                     <div class="price-discount">
                         <div class="old-price">
-                            <span class="whole">${products.price + products.saleAmount}</span>
-                            <span class="spearator">,</span>
-                            <span class="cents">${products.cents}</span>
+                        <span class="whole">${products.price + products.saleAmount}</span>
+                        <span class="spearator">,</span>
+                        <span class="cents">${products.cents}</span>
                         </div>
-
-                    </div>
-                </div>
-            </div>
-            <div class="installment"><span>${products.installment} zł x 20 rat 0%</span></div>
-            <div class="add-to-cart"><button class="add-to-cart-button"><span>DO
+                        
+                        </div>
+                        </div>
+                        </div>
+                        <div class="installment"><span>${products.installment} zł x 20 rat 0%</span></div>
+                        <div class="add-to-cart"><button class="add-to-cart-button"><span>DO
                         KOSZYKA</span></button></div>
-        </div>
-        `
+                        </div>
+                        `
 
 		productsSection.appendChild(newProduct)
 	})
 }
 
-const renderCategories = () => {
-	let categories = new Set(products.map(item => item.category))
+categoryItem.forEach((btn, index) => {
+	if (index !== 0) {
+		btn.addEventListener('click', function (e) {
+			const category = e.target.dataset.category
+			const selectedCategoryProducts = products.filter(item => {
+				if (item.category === category) return item
+			})
+			renderProducts(selectedCategoryProducts)
+			console.log(selectedCategoryProducts)
 
-	console.log(categories)
-	categories = ['polecamy', ...categories]
-	console.log(categories)
-}
+			categoryItem.forEach(item => item.classList.remove('active'))
+			this.classList.add('active')
+		})
+	}
+})
 
-document.onload = renderProducts(products)
+
+const recomendedProducts = products.filter(item => {
+    if (item.recomended === true) return item
+})
+
+recomendedBtn.addEventListener('click', function (e) {
+	renderProducts(recomendedProducts)
+})
+
+
+
+
+modelItem.forEach(btn =>
+	btn.addEventListener('click', function () {
+		modelItem.forEach(item => item.classList.remove('active'))
+		this.classList.add('active')
+	})
+)
+
 // document.onload = renderCategories(products)
+document.onload = renderProducts(recomendedProducts)
