a87f95184f35aa43655393ace6942792a4983440
ynzulak <maksymilian.a.kaluzny@gmail.com>
2023-04-23 13:52:08 +0200

    fixed and added a functional model divs with active class on eventListener

--- src/js/script.js
+++ src/js/script.js
@@ -20,34 +20,39 @@ const renderProducts = function (products) {
 		}`
 		newProduct.innerHTML = `
         <div class="product-header">
-            <h2>${products.name}</h2>
-            </div>
-            <div class="product-img"><img src="${products.image}"></div>
-            <div class="price-box">
-            <div class="full-price">
-            <div class="prices">
-            <div class="price-with-cents-and-currency"><span class="price">${products.price}</span>
-            <span class="cents-with-currency">
-            <span class="cents">${products.cents}</span>
-            <span class="'currency">zł</span></span>
-            </div>
-                    <div class="price-discount">
-                        <div class="old-price">
-                        <span class="whole">${products.price + products.saleAmount}</span>
-                        <span class="spearator">,</span>
-                        <span class="cents">${products.cents}</span>
-                        </div>
-                        
-                        </div>
-                        </div>
-                        </div>
-                        <div class="installment"><span>${products.installment} zł x 20 rat 0%</span></div>
-                        <div class="add-to-cart"><button class="add-to-cart-button"><span>DO
-                        KOSZYKA</span></button></div>
-                        </div>
-                        `
+		<h2>${products.name}</h2>
+		</div>
+		<div class="product-img"><img src="${products.image}"></div>
+		<div class="price-box">
+		<div class="full-price">
+		<div class="prices">
+		<div class="price-with-cents-and-currency"><span class="price">${products.price}</span>
+		<span class="cents-with-currency">
+		<span class="cents">${products.cents}</span>
+		<span class="'currency">zł</span></span>
+		</div>
+		<div class="price-discount">
+		<div class="old-price">
+		<span class="whole">${products.price + products.saleAmount}</span>
+		<span class="spearator">,</span>
+		<span class="cents">${products.cents}</span>
+		</div>
+		
+		</div>
+		</div>
+		</div>
+		<div class="installment"><span>${products.installment} zł x 20 rat 0%</span></div>
+		<div class="add-to-cart"><button class="add-to-cart-button"><span>DO
+		KOSZYKA</span></button></div>
+		</div>
+		`
+		const cartBtn = document.querySelectorAll('.add-to-cart-button')
 
 		productsSection.appendChild(newProduct)
+		// cartBtn.forEach(btn =>
+		// 	btn.addEventListener('click', e => {
+		// 		this
+		// 	}))
 	})
 }
 
@@ -61,16 +66,16 @@ const renderModels = function (products) {
 		newModel.innerHTML = `
 		<button class="btn"><span>${products}</span></button>
 		`
-
 		
-		newModel.addEventListener('click', e => {
-			const model = e.target.dataset.model
-			const selectedCategoryProducts = products.filter(item => {
-				if (item.category === category) return item
-			})
-		})
 		modelsList.appendChild(newModel)
 	})
+	const modelItem = document.querySelectorAll('.model-item')
+	modelItem.forEach(btn =>
+		btn.addEventListener('click', function (e) {
+			modelItem.forEach(item => item.classList.remove('active'))
+			this.classList.add('active')
+		})
+	)
 }
 
 modelItem.forEach(btn =>
