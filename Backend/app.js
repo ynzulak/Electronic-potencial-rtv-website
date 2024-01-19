d3cdde49fa83331d184f246fd2400b1241909092
[ynzulak] <maksymilian.a.kaluzny@gmail.com>
2024-01-19 20:36:54 +0100

    Adding backend (building API)

---
+++ Backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use('/api/images', express.static(path.join(__dirname, 'images')));
+const products = require('./data/products');
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.listen(port, () => {
+    console.log(`Serwer backendowy działa na http://localhost:${port}`);
+});
